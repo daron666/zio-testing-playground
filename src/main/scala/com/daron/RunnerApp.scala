2337a783d1b9af28b13c8fb7302655a31e0f7f0e
Alexey Terekhin <daron666@yandex.ru>
2021-12-02 13:14:31 +0100

    Some commit

--- src/main/scala/com/daron/RunnerApp.scala
+++ src/main/scala/com/daron/RunnerApp.scala
@@ -1,15 +1,13 @@
 package com.daron
 
-import zio.{ExitCode, URIO}
 import zio.console._
+import zio.{ExitCode, URIO}
 
 object RunnerApp extends zio.App {
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = myAppLogic.exitCode
 
   val myAppLogic =
     for {
-      _    <- putStrLn("Hello! What is your name?")
-      name <- getStrLn
-      _    <- putStrLn(s"Hello, ${name}, welcome to ZIO!")
+      _ <- putStrLn("Hello world!")
     } yield ()
 }
